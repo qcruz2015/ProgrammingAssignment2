6d79b98818f77b83d6f2b4fdc16b3c7e03e161cc
Q.Cruz <qcruz@admintech.us>
2015-07-26 20:06:15 -0400

    homework2

---
+++ makecachematrix.R
@@ -0,0 +1,13 @@
+
+
+makeMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
+}
